2e46b03e42b5d755acee9a0879c74ea0035db2d5
floowxy <alejsot1234@gmail.com>
2025-12-11 18:54:50 -0500

    Exercice 2.5 2.6 2.7

---
+++ Chapter 2/exercise2.5.6.7.c
@@ -0,0 +1,22 @@
+/*
+5.Which of the following are not legal C identifiers?
+(a) 100_bottles
+(b) _100_bottles
+(c) one__hundred__bottles
+(d) bottles_by_the_hundred_
+answer: a 
+Nunca puede empezar con un numero
+6. Why is it not a good idea for an identifier to contain more than one adjacent underscore (as
+in current___balance, for example)?
+
+porque el compilador de C puede utilizar esa variable __ como una herramienta 
+
+7. Which of the following are keywords in C?
+(a) for
+(b) If
+(c) main
+(d) printf
+(e) while
+answer : a,e
+Solo las instrucciones de control son keywords reservadas.
+*/
