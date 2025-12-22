6f452330804c65efeae8f72cd1e0f52d6feb170b
floowxy <alejsot1234@gmail.com>
2025-12-22 00:44:24 -0500

    p1

---
+++ Chapter 3/project1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main (void){
+    int day, month, year;
+    printf("Digit day : \n");
+    scanf("%d",&day);
+
+    printf("Digit month : \n");
+    scanf("%d",&month);
+    
+    printf("Digit year : \n");
+    scanf("%d",&year);
+
+
+    printf("%d%d%d\n",day,month,year);
+
+    return 0;
+}
